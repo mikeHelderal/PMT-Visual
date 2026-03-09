baa5eece574f64603b6c1ca15725fdddb633902e
mike helderal <mike.helderal.sio@gmail.com>
2026-03-09 18:48:39 +0100

    base

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import {Login} from './components/login/login';
+import {Register} from './components/register/register';
+
+export const routes: Routes = [
+  { path: 'register', component: Register },
+  { path: 'login', component: Login },
+  { path: '', redirectTo: 'login', pathMatch: 'full' } //
+];
